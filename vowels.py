28b775119c70614ae48f12d3646e7c9b6cc269b1
Shweta <shwetajagdhane06@gmail.com>
2022-02-11 15:13:56 +0530

    add vowels.py

--- vowels.py
+++ vowels.py
@@ -1,9 +1,11 @@
-vowels = ['a','e','i','o','u']
+vowels = ['a','e','i','o','u'] #given vowels list
 
-print("enter a character: \n")
+print("enter a character: \n")#ask for input
 
 c = input()
 
+#if character is in list vowels
+#print vowel else print consonant
 if c in vowels:
     print("Its a vowel")
 else:
