c4bae6c64287529445c6dad4e1bc33c43afc5adc
Shweta <shwetajagdhane06@gmail.com>
2022-06-21 20:35:33 +0530

    add encapsulation and student

--- educative/student.py
+++ educative/student.py
@@ -1,12 +1,12 @@
 class Student:
-    def __init__(self,name,phy,chem,bio):
+    def __init__(self,name,phy,chem,bio):  # initialized name, phy, chem, and bio as public properties in the class initializer.
         self.name = name
         self.phy = phy
         self.chem = chem
         self.bio = bio
 
-    def totalObtained(self):
-        return(self.phy + self.chem + self.bio)
+    def totalObtained(self): # defined the totalObtained() method. 
+        return(self.phy + self.chem + self.bio) #In this method, added individual marks of all the subjects and returned the sum.
 
-    def percentage(self):
-        return((totalObtained / 300) * 100)
+    def percentage(self):       # defined percentage(). In this method, called totalObtained() and used its value to calculate the percentage. 
+        return((totalObtained / 300) * 100)    #300 is the total number of marks for a student
