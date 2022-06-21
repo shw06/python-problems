66cc5db1fa76bf18050f1c4c657d032f99e3b658
Shweta <shwetajagdhane06@gmail.com>
2022-06-21 23:02:08 +0530

    add gettersetter.py

--- educative/encapsulation/gettersetter.py
+++ educative/encapsulation/gettersetter.py
@@ -1,17 +1,18 @@
-class Student:
+class Student:   #implemented the Student class, which has two private properties – __name and __rollNumber – 
+                 # that are initialized in the set methods.
     __name = None
     __rollNumber = None
     
-    def setName(self, name):
+    def setName(self, name):  # implemented setName(name), a method that sets the __name property.
         self.__name = name
 
-    def getName(self):
+    def getName(self):  #  implemented getName(), a method that returns the __name property.
         return self.__name
 
-    def setRollNumber(self, rollNumber):
+    def setRollNumber(self, rollNumber):  # implemented setRollNumber(RollNumber), a method that sets the __rollNumber property.
         self.__rollNumber = rollNumber
 
-    def getRollNumber(self):
+    def getRollNumber(self):  #  implemented getRollNumber(), a method that returns the __rollNumber property
         return self.__rollNumber
 
 obj = Student()
