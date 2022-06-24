a1b9e49e7a5e79093b30ea750af5e8d21e72f228
Shweta <shwetajagdhane06@gmail.com>
2022-06-25 00:49:54 +0530

    super.py

---
+++ educative/inheritance/super.py
@@ -0,0 +1,20 @@
+class Hospital:
+    def __init__(self,name,doctor):
+        self.name = name
+        self.doctor = doctor
+
+    def printHospital(self):
+        print("Hospital name: ", self.name)
+        print("Doctor name: ",self.doctor)
+
+class Department(Hospital):
+    def __init__(self, name, doctor,dname):   # constructor for child class
+        super().__init__(name,doctor)    # constructor of parent class
+        self.dname = dname
+
+    def printHospitalDept(self):
+        self.printHospital()
+        print("Department name: ", self.dname)
+
+obj1 = Department("City Hospital","Dr. Evans","Dept of Cardiology")
+obj1.printHospitalDept()
